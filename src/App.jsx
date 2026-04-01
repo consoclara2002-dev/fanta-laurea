052c350503ca25bf788094d00580d563f9f4214f
Clara Consonni <clara.consonni@studenti.unimi.it>
2026-04-01 15:11:11 +0200

    aggiunto stile h2

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ function App() {
   return (
     <div className="app-container">
       <div className="card">
-        <h1>Fanta Laurea 🎓</h1>
+        <h1>🎓FANTA LAUREA🎓</h1>
         <h2>La tesi è finita, la sfida comincia!</h2>
         <p className="subtitle">Seleziona le missioni che hai completato</p>
 
